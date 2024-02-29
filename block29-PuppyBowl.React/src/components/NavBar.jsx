ebf592148a0b7de644055ed7eff6db8844c65abb
katyjayne <katyjayneblank@gmail.com>
2024-02-29 17:14:42 -0500

    a few tiny tweaks

--- block29-PuppyBowl.React/src/components/NavBar.jsx
+++ block29-PuppyBowl.React/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function NavBar () {
   // TODO better - not generating new random numbers after we click
-  const id = Math.floor(Math.random() * 5 + 1);
+  const id = Math.floor(Math.random() * 10 + 1);
   return (
     <>
       {/* <h2>Nav Bar Component</h2> */}
